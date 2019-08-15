a95f66f34f0b04a46d4bdb55c109d9ddbb94f1f4
gitclone <gitclone@ocb.org.br>
2019-08-15 12:54:15 -0300

    Upload Source Code

--- src/main/java/br/com/example/config/ConfigApplication.java
+++ src/main/java/br/com/example/config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package br.com.saneago.config;
+package br.com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
